52ee81727ff20dbb91d65c0f6b4606ec44de661e
KasibM <syuriix@yahoo.ca>
2026-01-16 14:17:33 -0500

    lesson-2

--- 02/connect.php
+++ 02/connect.php
@@ -2,18 +2,21 @@
 
 declare(strict_types=1);
 
-$host = "localhost"; 
-$db = "test_connection"; 
-$user = "root"; 
-$pass = ""; 
+$host = "localhost"; //hostname
+$db = "test_connection"; //database name
+$user = "root"; //username
+$pass = ""; //password, "root" on windows, "" on mac
 
+//points to the database
 $dsn = "mysql:host=$host;dbname=$db";
 
+//try to connect, if connected echo a yay!
 try {
- $pdo = new PDO($dsn, $user, $pass); 
- $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
- echo "Connected to the database! Yay!"; 
+    $pdo = new PDO($dsn, $user, $pass); 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database! Yay!"; 
 }
+//what happens if there is an error connection
 catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
--- 02/index.php
+++ 02/index.php
@@ -1,7 +1,7 @@
 <?php
-//declare(strict_types=1);
-//1. Code Commenting 
-
+declare(strict_types=1);
+//1. Code Commenting jb
+require "connect.php";
 //inline code 
 
 /* 
@@ -30,53 +30,11 @@ if ($is_student === true) {
 
 //3. PHP is loosely typed 
 
-$num1 = 5;
-$num2 = "10";
+// $num1 = 7; //integer
+// $num2 = "9"; //string
 
-function add($num1, $num2) {
-    return $num1 + $num2;
-}
+// function add(int $num1,int $num2){
+//     return $num1, $num2;
+// }
 
-echo "<p>" . add($num1, $num2) . "</p>";
-
-//4. Strict Types & Types Hints
-/* Add declare(strict_types=1) and type hints 
-//strict types tell PHP not to automatically convert values when calling functions. Type hints tell PHP what to expect */
-
-/*function add(int $num1, int $num2): int {
-    return $num1 + $num2;
-}
-echo add($num1, $num2);
-*/
-
-//6. OOP with PHP 
-
-//create the class 
-
-class Person {
-    public string $name;
-    public int $age;
-    public bool $isInstructor;
-
-    public function __construct(string $name, int $age, bool $isInstructor)
-    {
-        $this->name = $name;
-        $this->age = $age;
-        $this->isInstructor = $isInstructor;
-    }
-
-    public function getBadge(): string
-    {
-        $role = $this->isInstructor ? "Instructor" : "Student";
-        return "Name :  {$this->name} | Age: {$this->age} | Role : $role";
-    }
-}
-
-//create an instance of object 
-
-$person = new Person("Jessica", 40, true);
-
-//use the object 
-
-echo $person->getBadge();
-
+// echo "<p>" . add($num1,$num2) . "</p>"
