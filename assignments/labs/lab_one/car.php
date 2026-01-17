402ea8ec9e755606facf44d33341ae6d3702a95b
KasibM <129322185+KasibM@users.noreply.github.com>
2026-01-17 14:10:42 -0500

    lab 1 up to step 3

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -1,20 +1,22 @@
-<?php
-declare(strict_types=1);
+<?php 
+declare(strict_types=1); //variable types must be declared
 
-class Car {
-    public string $make;
+class Car { 
+    // variable declarations
+    public string $make; 
     public string $model;
     public int $year;
+    // constructor
     public function __construct(string $make, string $model, int $year){
+        //passes arguments to this instance of Car object
         $this->make = $make;
         $this->model = $model;
         $this->year = $year;
     }
-
-    public function Describe(): string{
-        
-
+    // methods 
+    public function describe(): string{
+        // returns string with make, model and year information.
+        return "Make: {$this->make} | Model: {$this->model} | Year: {$this->year}";
     }
-
 }
 ?>
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -2,3 +2,9 @@
 require "header.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+require "car.php"; //ensures car.php is available for index.php to function
+
+$car1 = new Car("Nissan", "Pathfinder", 2011); //new instance of a car object
+echo $car1->describe(); //echo car information
+
+?>
