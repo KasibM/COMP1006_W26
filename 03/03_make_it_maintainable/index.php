14d44ceebea66d0c44c76a51a221357b2198d309
KasibM <129322185+KasibM@users.noreply.github.com>
2026-01-23 14:51:44 -0500

    lab two completed

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -6,29 +6,41 @@
 
     How can we refactor this code so it’s easier to maintain?
 */
+/* 
+    One thing I learned from this lab is that
+    php allows array literals to be passed as arguments.
+    I will likely use this to save time in Course Project
+    Phase One. 
+*/
+class HTMLList{
+    public $list; //list of items
+    public function __construct($list){ //constructs HTMLList object
+    $this->list = $list; // saves argument to $list
+    } 
+    public function displayList(): void { //function to display list in html
+        foreach ($this->list as $item): //loop through list items
+                echo "<li>" . $item . "</li>"; // display list item as li
+        endforeach; //end list loop
+    } 
+}
 
-$items = ["Home", "About", "Contact"];
+$items = new HTMLList (["Home", "About", "Contact"]); //new HTMLList object
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>My PHP Page</title>
-</head>
-<body>
-
-<h1>Welcome</h1>
-
-<ul>
-<?php foreach ($items as $item): ?>
-    <li><?= $item ?></li>
-<?php endforeach; ?>
-</ul>
-
-<footer>
-    <p>&copy; 2026</p>
-</footer>
-
-</body>
+    <head>
+        <title>My PHP Page</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <ul>
+            <?php  $items->displayList();  
+            //use displayList() function to display HTMLList object?> 
+        </ul>
+        <footer>
+            <p>&copy; 2026</p>
+        </footer>
+    </body>
 </html>
