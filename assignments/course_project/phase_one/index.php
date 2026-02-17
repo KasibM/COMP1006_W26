caa97c7a392f43563ebbc741a27b2672cf239b13
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-17 15:27:43 -0500

    initialise, skeleton and connection to bootstrap

---
+++ assignments/course_project/phase_one/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+
+
+
+?>
+
+<?php 
+require 'includes/connect.php';
+require 'includes/header.php';
+?>
+
+<main>
+    <h1>Time Tracker</h1>
+</main>
+
+<?php require 'includes/footer.php'; ?>
+
+
+
+
