1a8ebc68bff3fefbf92a6ac7ca23501452c62ae7
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-19 18:02:03 -0500

    finished basic formatting on add task page

--- assignments/course_project/phase_one/index.php
+++ assignments/course_project/phase_one/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 
@@ -7,54 +7,68 @@
 
 ?>
 
-<?php 
+<?php
 require 'includes/header.php';
 ?>
 
 <main>
-    <h1>Time Tracker</h1>
-    <form action="add-task-process.php">
-        <fieldset>
-            <legend>Add Task</legend>
-
-            <label for="task_name">Task Name</label>
-            <input type="text" id="task_name" name="task_name" require/>
-
-            <label for="task_category">Category</label>
-            <input type="text" id="task_category" name="task_category" require/>
-
-            <label for="task_priority">Priority</label>
-            <select id="task_priority" name="task_priority" require>
-                <option value="High">High</option>
-                <option value="Medium">Medium</option>
-                <option value="Low">Low</option>
-            </select>
-            
-            <label for="task_due_date">Due Date</label>
-            <input type="date" id="task_due_date" name="task_due_date" require/>
-
-            <label for="task_time">Time in Hours Spent</label>
-            <input type="number" id="task_time" name="task_time" step="0.5" min="0.0" max="12.0" placeholder="Hour(s)"/>
-            
-            <!-- <label for="task_status">Completed</label> -->
-            <label for="task_status">Task Status</label>
-            <div class="form-check form-check">
-                <input class="form-check-input" type="radio" role="switch" id="task_status_incomplete" name="task_status" value="0" require/>
-                <label class="form-check-label" for="task_status_default">Incomplete</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" role="switch" id="task_status_complete" name="task_status" value="1" require/>
-                <label class="form-check-label" for="task_status_checked">Complete</label>
-            </div>
-        </fieldset>
-        <p>
-        <button type="submit">Add Task</button>
-        </p>
-    </form>
+    <div class = "container-sm">
+        <form action="add-task-process.php" method="post">
+            <fieldset>
+                <br>
+                <legend>Add Task</legend>
+                <!-- Have to add line breaks because even though main.css (after bootstrap in header.php should outrank reboot.scss, bootstrap won't let me change anything-->
+                <br>
+                <br>
+                <!-- Name  -->
+                <label for="task_name" class="form-label">Task Name</label>
+                <input type="text" id="task_name" name="task_name" class="form-control" required />
+                <br>
+                <!-- Category  -->
+                <label for="task_category" class="form-label">Category</label>
+                <input type="text" id="task_category" name="task_category" class="form-control" required />
+                <br>
+                <!-- Priority  -->
+                <label for="task_priority" class="form-label">Priority</label>
+                <select id="task_priority" name="task_priority" class="form-control" required>
+                    <option value="">Select</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+                <br>
+                <!-- Due Date  -->
+                <label for="task_due_date" class="form-label">Due Date</label>
+                <input type="date" id="task_due_date" name="task_due_date" class="form-control" required />
+                <br>
+                <!-- Task Time  -->
+                <label for="task_time" class="form-label">Time in Hours Spent</label>
+                <input type="number" id="task_time" name="task_time" step="0.5" min="0.0" max="12.0" placeholder="Hour(s)" class="form-control" />
+            </fieldset>
+            <br>
+            <fieldset>
+                <!-- Task Status  -->
+                <legend>Task Status</legend>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" id="task_status_incomplete" name="task_status" value="0" required />
+                    <label class="form-check-label" for="task_status_incomplete">Incomplete</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" id="task_status_complete" name="task_status" value="1" required />
+                    <label class="form-check-label" for="task_status_complete">Complete</label>
+                </div>
+            </fieldset>
+            <br>
+            <!-- Submit Button  -->
+            <p>
+                <button type="submit">Add Task</button>
+                
+            </p>
+            <br>
+            <br>
+            <br>
+        </form>
+    </div>
 </main>
 
 <?php require 'includes/footer.php'; ?>
-
-
-
-
