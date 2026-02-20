876095607bb4cb19dba3d5664b89653aef31b3bc
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-19 19:19:21 -0500

    add drop table if exists to replace previous versions

--- assignments/course_project/phase_one/time_tracker_initialise.sql
+++ assignments/course_project/phase_one/time_tracker_initialise.sql
@@ -1,10 +1,11 @@
+DROP TABLE IF EXISTS tasks;
 CREATE TABLE tasks (
   id INT AUTO_INCREMENT PRIMARY KEY,
   task_name VARCHAR(100),
   task_category  VARCHAR(100),
-  task_priority ENUM('Low','Medium','High'),
-  task_due_date DATE,
+  task_priority VARCHAR(6),
+  task_due_date CHAR(10),
   task_time FLOAT(3,1),
   task_status BOOLEAN,
-  subscribed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  task_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
