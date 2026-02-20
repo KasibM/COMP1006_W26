6da59787b725c7550c650e5ec0936b39b4c3c785
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-20 00:02:01 -0500

    filled out view all task with table, and some data format conversions

--- assignments/course_project/phase_one/view-all-tasks.php
+++ assignments/course_project/phase_one/view-all-tasks.php
@@ -25,6 +25,52 @@ $_pdo = null;
     <div class = "container-sm">
         <br>
         <h2>Tasks</h2>
+
+        <table class="table table-bordered mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Priority</th>
+                    <th>Due Date</th>
+                    <th>Time Spent (h)</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($tasks as $task): ?>
+                    <tr>
+                        <td>
+                            <?= htmlspecialchars($task['id']);?>
+                        </td>
+                        <td>
+                            <?= htmlspecialchars($task['task_name']);?>
+                        </td>
+                        <td>
+                            <?= htmlspecialchars($task['task_category']);?>
+                        </td>
+                        <td>
+                            <?= htmlspecialchars($task['task_priority']);?>
+                        </td>
+                        <td>
+                            <!-- converts YYYY-MM-DD to Month (short) day, year -->
+                            <?= htmlspecialchars(date("M d, Y", strtotime($task['task_due_date'])));?>
+                        </td>
+                        <td>
+                            <?= htmlspecialchars($task['task_time']);?>
+                        </td>
+                        <td>
+                            <?php if ($task['task_status'] === "1" ): ?>
+                                Complete
+                            <?php else: ?>
+                                Incomplete
+                            <?php endif;?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
         
         
         
