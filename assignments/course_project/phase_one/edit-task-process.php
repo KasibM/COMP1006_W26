3a8430d6422befd9f3a9f4657f756ed4f72094b1
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-20 17:32:04 -0500

    building based on index.html form

---
+++ assignments/course_project/phase_one/edit-task-process.php
@@ -0,0 +1,93 @@
+<?php
+require "includes/connect.php";
+require 'includes/header.php';
+
+//check if post
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    die('Invalid request');
+}
+
+$selectedTask = trim(filter_input(INPUT_POST,'task_id',FILTER_SANITIZE_SPECIAL_CHARS));
+
+
+//build query with named placeholder 
+$sql = "SELECT * FROM tasks WHERE task_id = :selected_task";
+
+//prepare the query
+$stmt = $pdo->prepare($sql);
+
+//bind param to placeholder
+$stmt -> bindParam(':selected_task', $selectedTask);
+
+//execute the query
+$stmt -> execute();
+
+//fetch query results
+$tasks = $stmt->fetchALL();
+
+//close connection
+$_pdo = null;
+
+?>
+
+<main>
+    <div class = "container-sm">
+        <form action="add-task-process.php" method="post">
+            <fieldset>
+                <br>
+                <legend>Edit Task</legend>
+                <!-- Have to add line breaks because even though main.css (after bootstrap in header.php should outrank reboot.scss, bootstrap won't let me change anything, update: removed main.css -->
+                <br>
+                <br>
+                <!-- Name  -->
+                <label for="task_name" class="form-label">Task Name</label>
+                <input type="text" id="task_name" name="task_name" class="form-control" />
+                <br>
+                <!-- Category  -->
+                <label for="task_category" class="form-label">Category</label>
+                <input type="text" id="task_category" name="task_category" class="form-control" />
+                <br>
+                <!-- Priority  -->
+                <label for="task_priority" class="form-label">Priority</label>
+                <select id="task_priority" name="task_priority" class="form-control" >
+                    <option value="">Select</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                </select>
+                <br>
+                <!-- Due Date  -->
+                <label for="task_due_date" class="form-label">Due Date</label>
+                <input type="date" id="task_due_date" name="task_due_date" class="form-control"  />
+                <br>
+                <!-- Task Time  -->
+                <label for="task_time" class="form-label">Time in Hours Spent</label>
+                <input type="number" id="task_time" name="task_time" step="0.5" min="0.0" max="12.0" placeholder="Hour(s)" class="form-control" />
+            </fieldset>
+            <br>
+            <fieldset>
+                <!-- Task Status  -->
+                <legend>Task Status</legend>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" id="task_status_incomplete" name="task_status" value="0" required />
+                    <label class="form-check-label" for="task_status_incomplete">Incomplete</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" id="task_status_complete" name="task_status" value="1" />
+                    <label class="form-check-label" for="task_status_complete">Complete</label>
+                </div>
+            </fieldset>
+            <br>
+            <!-- Submit Button  -->
+            <p>
+                <button type="submit">Add Task</button>
+                
+            </p>
+            <br>
+            <br>
+            <br>
+        </form>
+    </div>
+</main>
+
+<?php require 'includes/footer.php'; ?>
