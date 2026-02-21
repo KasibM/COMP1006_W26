35628144d43ac93e987fa2322cc7db96a9adeb42
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-20 19:40:48 -0500

    removed unrelated

--- assignments/course_project/phase_one/update-task-process.php
+++ assignments/course_project/phase_one/update-task-process.php
@@ -9,6 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 //get + sanitise values
+$taskID      = trim(filter_input(INPUT_POST,'task_id',FILTER_SANITIZE_SPECIAL_CHARS));
 $taskName      = trim(filter_input(INPUT_POST,'task_name',FILTER_SANITIZE_SPECIAL_CHARS));
 $taskCategory  = trim(filter_input(INPUT_POST,'task_category',FILTER_SANITIZE_SPECIAL_CHARS));
 $taskPriority  = trim(filter_input(INPUT_POST,'task_priority',FILTER_SANITIZE_SPECIAL_CHARS));
@@ -21,33 +22,23 @@ $taskStatus    = trim(filter_input(INPUT_POST,'task_status',FILTER_SANITIZE_SPEC
 
 $errors = [];
 
-if($taskName === null || $taskName === ''){
-    $errors[]= "task_name is required.";
+if($taskID === null || $taskID === ''){
+    $errors[] = " task_id is required.";
 }
 
-if($taskCategory === null || $taskCategory === ''){
-    $errors[]= "task_category is required";
+if ($taskDueDate === null || $taskCategory === ''){
+
+} else if (!filter_var($taskDueDate, FILTER_VALIDATE_REGEXP, ['options'=> ['regexp' => '/^\d{4}-\d{2}-\d{2}$/']] )){
+    $errors[] = "task_due_date is not in required format expected: 'YYYY-MM-DD', received: '".$taskDueDate."'.";
 }
 
-if($taskPriority !== "Low" && $taskPriority !== "Medium" && $taskPriority !== "High"){
-    $errors[]= "task_priority must be 'Low', 'Medium', or 'High";
-}
-
-if($taskDueDate === null || $taskDueDate === ''){
-    $errors[]= "task_due_date is required.";
-} else if(!filter_var($taskDueDate, FILTER_VALIDATE_REGEXP, ['options'=> ['regexp' => '/^\d{4}-\d{2}-\d{2}$/']] )){
-    $errors[]= "task_due_date is not in required format expected: 'YYYY-MM-DD', received: '".$taskDueDate."'.";
+if($taskTime === null || $taskTime === ''){
+    
+} else if (!filter_var($taskTime, FILTER_VALIDATE_FLOAT)){
+    $errors[] = "task_time must be a float.";
 } 
 
-if($taskTime === null || $taskTime ===''){
-    $taskTime= 0;
-} else if(!filter_var($taskTime, FILTER_VALIDATE_FLOAT)){
-    $errors[]= "task_time must be a float.";
-}
 
-if(!($taskStatus === '0' || $taskStatus === '1')){
-    $errors[]= "task_status must be 0 or 1.";
-}
 
 //if errors stop before inserting into the database
 if (!empty($errors)) { ?>
@@ -59,8 +50,17 @@ if (!empty($errors)) { ?>
     exit;
 }
 
+
+
+
+
+
+
+
+
+
 //build query with named placeholder 
-$sql = "INSERT INTO tasks (task_name, task_category, task_priority, task_due_date, task_time, task_status) VALUES (:task_name, :task_category, :task_priority, :task_due_date, :task_time, :task_status)";
+$sql = "UPDATE tasks SET WHERE id = :task_id";
 
 //prepare the query
 $stmt = $pdo->prepare($sql);
@@ -68,6 +68,7 @@ $stmt = $pdo->prepare($sql);
 //map named placeholder to data
 //e.g. $stmt -> bindParam(":first_name", firstName);
 
+$stmt -> bindParam(':task_id', $taskID);
 $stmt -> bindParam(':task_name', $taskName);
 $stmt -> bindParam(':task_category', $taskCategory);
 $stmt -> bindParam(':task_priority', $taskPriority);
@@ -75,14 +76,16 @@ $stmt -> bindParam(':task_due_date', $taskDueDate);
 $stmt -> bindParam(':task_time', $taskTime);
 $stmt -> bindParam(':task_status', $taskStatus);
 
+
 //execute the query
-$stmt -> execute();
+//$stmt -> execute();
 
 //close connection
 $_pdo = null;
 
 
-//translate $taskStatus variable for display
+
+
 $taskStatusWord;
 if ($taskStatus === "1") {
     $taskStatusWord = "Yes";
@@ -95,7 +98,7 @@ if ($taskStatus === "1") {
 <main>
     <div class = "container-sm">
         <br>
-        <h2>Task Added</h2>
+        <h2>Task Updated</h2>
         <p><?php echo "Task: ".$taskName.", from ".$taskCategory." was added successfully.
         <br>Priority: ".$taskPriority.
         "<br>Due: ".$taskDueDate.
@@ -103,7 +106,7 @@ if ($taskStatus === "1") {
         "<br>Complete: ".$taskStatusWord?>
         </p>
         <p>
-            <a href="index.php" class="nav-link-active">New Task</button>
+            <a href="index.php" class="nav-link-active">Edit New Task</a>
         </p>
         
     </div>
