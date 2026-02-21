0e1e06977ea31c80eb4e389df9df9209afa4df8e
KasibM <129322185+KasibM@users.noreply.github.com>
2026-02-20 20:24:22 -0500

    removed unrelated data, changed links/query/labels

--- assignments/course_project/phase_one/remove-task-process.php
+++ assignments/course_project/phase_one/remove-task-process.php
@@ -9,13 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 //get + sanitise values
-$taskID      = trim(filter_input(INPUT_POST,'task_id',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskName      = trim(filter_input(INPUT_POST,'task_name',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskCategory  = trim(filter_input(INPUT_POST,'task_category',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskPriority  = trim(filter_input(INPUT_POST,'task_priority',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskDueDate  = trim(filter_input(INPUT_POST,'task_due_date',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskTime      = trim(filter_input(INPUT_POST,'task_time',FILTER_SANITIZE_SPECIAL_CHARS));
-$taskStatus    = trim(filter_input(INPUT_POST,'task_status',FILTER_SANITIZE_SPECIAL_CHARS));
+$taskID = trim(filter_input(INPUT_POST,'task_id',FILTER_SANITIZE_SPECIAL_CHARS));
 
 
 //validation
@@ -26,21 +20,9 @@ if($taskID === null || $taskID === ''){
     $errors[] = " task_id is required.";
 }
 
-if ($taskDueDate === null || $taskCategory === ''){
-
-} else if (!filter_var($taskDueDate, FILTER_VALIDATE_REGEXP, ['options'=> ['regexp' => '/^\d{4}-\d{2}-\d{2}$/']] )){
-    $errors[] = "task_due_date is not in required format expected: 'YYYY-MM-DD', received: '".$taskDueDate."'.";
-}
-
-if($taskTime === null || $taskTime === ''){
-    
-} else if (!filter_var($taskTime, FILTER_VALIDATE_FLOAT)){
-    $errors[] = "task_time must be a float.";
-} 
-
 //if errors stop before inserting into the database
 if (!empty($errors)) { ?>
-    <?php echo "Failed to insert data due to the following errors:\n";
+    <?php echo "Failed to remove data due to the following errors:\n";
     foreach ($errors as $error) : ?>
         <li><?php echo $error; ?> </li>
 <?php endforeach;
@@ -48,54 +30,9 @@ if (!empty($errors)) { ?>
     exit;
 }
 
-//since editing has optional field, must retrieve old data to resubmit if it is not changed, or else PDO has a fit
-
 
 //build query with named placeholder 
-$sql = "SELECT * FROM tasks WHERE id = :selected_task";
-
-//prepare the query
-$stmt = $pdo->prepare($sql);
-
-//bind param to placeholder
-$stmt -> bindParam(':selected_task', $taskID);
-
-//execute the query
-$stmt -> execute();
-
-//fetch query results
-$task = $stmt->fetch();
-
-
-//replace empty with original values
-if($taskName === null || $taskName === ''){
-    $taskName = $task['task_name'];
-}
-if($taskCategory === null || $taskCategory === ''){
-    $taskCategory = $task['task_category'];
-}
-if($taskPriority === null || $taskPriority === ''){
-    $taskPriority = $task['task_priority'];
-}
-if($taskDueDate === null || $taskDueDate === ''){
-    $taskDueDate = $task['task_due_date'];
-}
-if($taskTime === null || $taskTime === ''){
-    $taskTime= $task['task_time'];
-}
-if($taskStatus === null || $taskStatus === ''){
-    $taskStatus = $task['task_status'];
-}
-
-//build query with named placeholder 
-$sql = "UPDATE tasks 
-        SET task_name = :task_name,
-            task_category = :task_category,
-            task_priority = :task_priority,
-            task_due_date = :task_due_date,
-            task_time = :task_time,
-            task_status = :task_status
-        WHERE id = :task_id";
+$sql = "DELETE from tasks WHERE id = :task_id";
 
 //prepare the query
 $stmt = $pdo->prepare($sql);
@@ -104,12 +41,6 @@ $stmt = $pdo->prepare($sql);
 //e.g. $stmt -> bindParam(":first_name", firstName);
 
 $stmt -> bindParam(':task_id', $taskID);
-$stmt -> bindParam(':task_name', $taskName);
-$stmt -> bindParam(':task_category', $taskCategory);
-$stmt -> bindParam(':task_priority', $taskPriority);
-$stmt -> bindParam(':task_due_date', $taskDueDate);
-$stmt -> bindParam(':task_time', $taskTime);
-$stmt -> bindParam(':task_status', $taskStatus);
 
 
 //execute the query
@@ -119,27 +50,16 @@ $stmt -> execute();
 $_pdo = null;
 
 
-$taskStatusWord;
-if ($taskStatus === "1") {
-    $taskStatusWord = "Yes";
-} else {
-    $taskStatusWord = "No";
-}
 ?>
 <!-- display in html -->
  
 <main>
     <div class = "container-sm">
         <br>
-        <h2>Task Updated</h2>
-        <p><?php echo "Task: ".$taskName.", from ".$taskCategory." was added successfully.
-        <br>Priority: ".$taskPriority.
-        "<br>Due: ".date("M d, Y", strtotime($taskDueDate)).
-        "<br>Hours added: ".$taskTime.
-        "<br>Complete: ".$taskStatusWord?>
-        </p>
+        <h2>Task Removed Successfully.</h2>
+        <br>
         <p>
-            <a href="select-task-edit.php" class="nav-link-active">Edit New Task</a>
+            <a href="select-task-remove.php" class="nav-link-active">Remove New Task</a>
         </p>
         
     </div>
